2fe51f3ec29d886caa07c37bd5efaaa966335b7a
Victor Rejano <victorrejanoapps@gmail.com>
2018-12-23 15:47:46 +0100

    [ADD] Script to install db

---
+++ scripts/installDB.js
@@ -0,0 +1,67 @@
+'use strict';
+require('../lib/connection');
+const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
+// Modelo
+const Ad = require('../models/Ad');
+const User = require('../models/User');
+
+// Eliminar colecciones existentes
+Ad.collection.drop();
+User.collection.drop();
+
+// Leer datos de prueba de anuncios
+let filePath = path.join('mocks', 'ads.json');
+fs.readFile(filePath, {encoding: 'utf8'}, async (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    try {
+        // Extraer datos de json
+        data = await JSON.parse(data);
+
+        // Cargar datos de anuncios
+        await Ad.collection.insertMany(data.ads, (err, doc) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        });
+
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+// Cargar datos de prueba de usuario
+filePath = path.join('mocks', 'users.json');
+fs.readFile(filePath, {encoding: 'utf8'}, async (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    try {
+        // Extraer datos de json
+        data = await JSON.parse(data);
+
+        // Cargar datos de usuario
+        data.users.forEach((element) => {
+            const user = new User(element);
+
+            // Hashear password
+            const salt = bcrypt.genSaltSync(process.env.HASH_SALT);
+            const hash = bcrypt.hashSync(user.password, salt);
+            user.password = hash;
+
+            // Almacenar usuario
+            user.save();
+        });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+
